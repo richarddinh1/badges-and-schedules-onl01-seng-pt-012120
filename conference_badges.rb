b73a7abd4e2d8ca62b9264c44593a8dc882afd32
richard dinh <richardndinh@gmail.com>
2020-01-29 19:34:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,15 @@ def batch_badge_creator(name)
   return newarray
 end
 
-
+def assign_rooms(array)
+  newarray = []
+  counter = 1
+  array.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return newarray
+end
 
 
 def printer(attendees)
