d5ff4e66a254689281bee7d19f0f64aff9e9fdc9
richard dinh <richardndinh@gmail.com>
2020-01-29 19:33:04 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -109,16 +109,3 @@ TEXT
 
 end
 
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(name)
-  newarray = [ ]
-  array.each do |name|
-    newarray.push("Hello, my name is #{name}")
-  end
-  return newarray
-end
