104ea7d93c13556a5816dc941ece64b65d516803
richard dinh <richardndinh@gmail.com>
2020-01-29 19:15:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+
+
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+end
+    assing_rooms(attendees).each do |attendee|
+      puts attendee
+  end
+end
